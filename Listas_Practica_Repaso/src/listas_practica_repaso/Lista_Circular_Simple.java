f00ac9ef8e878787a0038911042db313981cb131
Ruben Alejandro Caballeros Prado <2999062130101@ingenieria.usac.edu.gt>
2022-02-03 16:22:55 -0600

    solo nodo circular simple

--- Listas_Practica_Repaso/src/listas_practica_repaso/Lista_Circular_Simple.java
+++ Listas_Practica_Repaso/src/listas_practica_repaso/Lista_Circular_Simple.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package listas_practica_repaso;
 
-/**
- *
- * @author Ruben
- */
+
 public class Lista_Circular_Simple {
+    
+    
+    
     
 }
---
+++ Listas_Practica_Repaso/src/listas_practica_repaso/Nodo_Circular_Simple.java
@@ -0,0 +1,12 @@
+
+package listas_practica_repaso;
+
+
+public class Nodo_Circular_Simple {
+    
+    //creo el nodo con un objeto tipo estudiante
+    Estudiante estudiante=new Estudiante();
+    Nodo_Circular_Simple Siguiente;
+    
+    
+}
